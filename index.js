5a96b4bb505a17a0de850d9b647fbf5357067c3c
Maya <maya.hubbard@outlook.com>
2025-07-02 17:53:40 -0700

    buttons show/hide based on gameplay

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // let player = {
 //     name: "Maya",
 //     chips: 200
@@ -15,27 +14,33 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
-let playerEl = document.getElementById("player-el");
+let startBtnEl = document.getElementById("start-btn")
+let newBtnEl = document.getElementById("new-btn");
+let restartBtnEl = document.getElementById("restart-btn")
+// let playerEl = document.getElementById("player-el");
 
-playerEl.textContent = player.name + ": $"+ player.chips;
+// playerEl.textContent = player.name + ": $"+ player.chips;
 
 function getRandomCard() {
-    let randomCard = Math.floor(Math.random() * 13) +1;
-    if (randomCard === 1) {
-        return 11;
-    } else if (randomCard > 10) {
-        return 10;
-    }
-    return randomCard;
+  let randomCard = Math.floor(Math.random() * 13) + 1;
+  if (randomCard === 1) {
+    return 11;
+  } else if (randomCard > 10) {
+    return 10;
+  }
+  return randomCard;
 }
 
 function startGame() {
-    isAlive = true;
-    let firstCard = getRandomCard();
-    let secondCard = getRandomCard();
-    cards = [firstCard,secondCard];
-    sum = firstCard + secondCard;
-    renderGame();
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+  newBtnEl.style.display = "block";
+  startBtnEl.style.display = "none";
+  restartBtnEl.style.display = "none";
 }
 
 function renderGame() {
@@ -43,25 +48,34 @@ function renderGame() {
   for (i = 0; i < cards.length; i++) {
     cardsEl.textContent += cards[i] + " ";
   }
-
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
     message = "You've got Blackjack!";
     hasBlackJack = true;
+    restartBtnEl.style.display = "block";
+    newBtnEl.style.display = "none";
   } else {
     message = "You're out of the game!";
     isAlive = false;
+    restartBtnEl.style.display = "block";
+    newBtnEl.style.display = "none";
   }
   messageEl.textContent = message;
 }
 
 function newCard() {
-    if (isAlive === true && hasBlackJack === false) {
-        let card = getRandomCard();
-        sum += card;
-        cards.push(card);
-        renderGame();
-    }
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
+
+  function restartGame() {
+    cardsEl.textContent = "";
+    sumEl.textContent = "";
+    startGame();
+  }
